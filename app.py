a498ce51f0e9892a446c7b711b4c288079d09967
orqidea core <contacto.orqidea@gmail.com>
2025-11-11 19:16:32 -0500

    Version 1.2 Se agrego validacion de errores, bitacra de ejecucion en archivo .log
    Signed-off-by: orqidea core <contacto.orqidea@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
+import datetime
 import os
-# app.py
+
+def log(mensaje):
+    with open("bitacora.log", "a") as f:
+        f.write(f"{datetime.datetime.now()} - {mensaje}\n")
+
 def celsius_a_fahrenheit(c):
-    """Convierte Celsius a Fahrenheit"""
-    return (c * 9/5) + 32
+    resultado = (c * 9/5) + 32
+    log(f"Convertido {c}°C → {resultado}°F")
+    return resultado
 
 def fahrenheit_a_celsius(f):
-    """Convierte Fahrenheit a Celsius"""
-    return (f - 32) * 5/9
+    resultado = (f - 32) * 5/9
+    log(f"Convertido {f}°F → {resultado}°C")
+    return resultado
 
 if __name__ == "__main__":
     print("Conversor de temperaturas")
-    c = float(input("Ingrese grados Celsius: "))
-    print(f"{c}°C equivalen a {celsius_a_fahrenheit(c)}°F")
+    opcion = input("Seleccione (1) celsius→fahrenheit o (2) F→C: ")
+    if opcion == "1":
+        c = float(input("Ingrese grados Celsius: "))
+        print(f"{c}°celsius = {celsius_a_fahrenheit(c)}°F")
+    elif opcion == "2":
+        f = float(input("Ingrese grados Fahrenheit: "))
+        print(f"{f}°fahrenheit = {fahrenheit_a_celsius(f)}°celsius")
+    else:
+        print("Opción no válida")
